2edc3bf735bac7fcea961e7326e2b06687b74ad3
Anderson S. Silva <andsoares2001@gmail.com>
2021-10-17 17:14:56 -0300

    Create exercicio8.java

---
+++ Lista1/exercicio8.java
@@ -0,0 +1,10 @@
+package lista1;
+
+public class classedoC {
+	public int cont;
+	public void classedoC() {
+		//constructor
+		this.cont = cont + 1;//contabiliza as vezes em que a variavel C foi resetada
+		System.out.println("a variavel c foi reiniciada "+cont+" vezes.\n");
+	}
+}
