b04cb83bbd21598285d04c70e8649f5827e95fae
Mariana Gonçalves Rodrigues <mary.judo@gmail.com>
2021-10-18 16:31:33 -0300

    Update exercicio8.java

--- Lista1/exercicio8.java
+++ Lista1/exercicio8.java
@@ -1,3 +1,10 @@
+/**
+* @author Mariana Goncalves Rodrigues, Anderson Soares da Silva
+* @r.a: 2151014 e 2150875
+* @date 04 OUT 2021
+* @brief a classedoC é a nova classe que já está inserida no exercicio 7
+*/
+
 package lista1;
 
 public class classedoC {
