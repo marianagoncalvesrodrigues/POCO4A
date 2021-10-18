1ad0f4d35fc6b1b5ca175a89947fe0013a0473d3
Anderson S. Silva <andsoares2001@gmail.com>
2021-10-18 15:32:47 -0300

    Update exercicio9.java

--- Lista1/exercicio9.java
+++ Lista1/exercicio9.java
@@ -10,10 +10,9 @@ public class Principal9 {
 	
 	public static void main(String [] args) {
 		Computador9 c = new Computador9();
-		c.setNome("comp1").setMarca("Intel").setData(1, 1, 1989);
+		c.setNome("comp1").setMarca("Intel").setData(1,1,2001);
 		System.out.println(c.toString());
 	}
-
 }
 
 package lista1;
@@ -21,17 +20,10 @@ package lista1;
 public class Computador9 {
 	public String nome, marca;
 	public int dia, mes, ano;
-	Data9 aux = new Data9();
+	public Data9 aux =  new Data9();
 	public Computador9() {
 		//constructor
 	}
-	
-	public Computador9(String nome, String marca) {
-		setNome(nome);
-		setMarca(marca);
-		setData(dia, mes, ano);
-	}
-	
 
 	public Computador9 setNome(String nome) {
 		this.nome = nome;
@@ -41,31 +33,18 @@ public class Computador9 {
 		this.marca = marca;
 		return this;
 	}
-	/*
-	public Data9 auxsetData(int dia, int mes, int ano) {
-		return this.aux.setData(dia, mes, ano); 
-	}*/
-	
 	public Computador9 setData(int dia, int mes, int ano) {
-		/*this.dia = dia;
-		this.mes = mes;
-		this.ano = ano;
+		this.aux.Data(dia, mes, ano);
 		return this;
-		*/
-		aux.data(this.dia = dia, this.mes = mes, this.ano = ano);
-		return this;
-		
 	}
-	
 	public String toString(){
 		return "NOME: " + this.nome +
 				"\nMARCA: " + this.marca +
-				"\nDATA: " + aux.data(dia, mes, ano);			
+				"\nDATA: " +  this.aux.dia + this.aux.mes + this.aux.ano;			
 	}
-
 }
+
 package lista1;
-import java.text.DateFormat;
 public class Data9 {
 	public int dia;
 	public int mes;
@@ -75,14 +54,10 @@ public class Data9 {
 		//consctructor
 	}
 	
-	public Data9 data(int dia, int mes, int ano){
+	public void Data(int dia, int mes, int ano){
 		this.dia = dia;
 		this.mes = mes;
 		this.ano = ano;
-		/*
-		DateFormat f = DateFormat.getDateInstance();
-		*/
-		return this;
 	}
-
 }
+
