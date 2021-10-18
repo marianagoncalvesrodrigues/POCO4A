034d96a542488b1f0a0b04636af1f8ee10168164
Mariana Gonçalves Rodrigues <mary.judo@gmail.com>
2021-10-18 16:28:00 -0300

    Update exercicio6.java

--- Lista1/exercicio6.java
+++ Lista1/exercicio6.java
@@ -1,3 +1,8 @@
+/**
+* @author Mariana Goncalves Rodrigues, Anderson Soares da Silva
+* @r.a: 2151014 e 2150875
+* @date 04 OUT 2021
+*/
 package lista1;
 
 public class novaClasse {//verifica se o numero aleatorio é primo, beneficio acionado ao chegar em 3 tentativas erradas
